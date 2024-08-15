b9a279024f0229fcc44f32467e038be6806c296b
al-amin90 <ijesun30@gmail.com>
2024-08-16 05:09:14 +0600

    add: express setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+
+
 require('dotenv').config()
+const port = 3000 || false;
+
+app.get("/", (req, res) => {
+    res.send("InnoMart server is running Perfectly...")
+})
+
+app.listen(port, () => {
+    console.log(`the server port is running on ${port}`);
+})
